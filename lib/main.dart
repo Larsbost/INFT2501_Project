08312e0a71ec526622b4d06e17bc9b5ea554e513
Lars Brodin Østby <larsbost@stud.ntnu.no>
2021-11-10 15:09:19 +0100

    Added l10n

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:inft2501_prosjekt/pages/main_menu.dart';
 import 'package:inft2501_prosjekt/pages/hangman_game.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const Hangman());
@@ -14,6 +15,16 @@ class Hangman extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+        Locale('nb', ''), // Spanish, no country code
+      ],
       initialRoute: "/home",
       routes: {
         '/home' : (context) => MainMenu(),
